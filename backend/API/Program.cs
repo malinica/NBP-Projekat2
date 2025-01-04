0801873043d03195ff09011e894cb22e3a820885
unknown <malinovicjovana13@gmail.com>
2025-01-04 13:41:28 +0100

    Napravljen projekat i backend za rad sa swagger-om

---
+++ backend/API/Program.cs
@@ -0,0 +1,18 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.UseSwaggerUI(options => {
+        options.SwaggerEndpoint("/openapi/v1.json","API");
+    });
+}
+
+app.UseHttpsRedirection();
+
+app.Run();
