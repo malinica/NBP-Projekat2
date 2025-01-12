1a14f2e5cecf88c2faef77e866fc5faf613a58a2
unknown <malinovicjovana13@gmail.com>
2025-01-12 15:44:33 +0100

    Dodata komponenta UserCard, koja je povezana na stranici pretrage korisnika i popravljen font dugmeta

--- frontend/src/Components/SearchUserPage/SearchUserPage.tsx
+++ frontend/src/Components/SearchUserPage/SearchUserPage.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { getAllUsersAPI } from "../../Services/UserService";
 import { User } from "../../Interfaces/User/User";
+import UserCard from "../UserCard/UserCard";
 
 const SearchUserPage = () => {
     const [users, setUsers] = useState<User[] | undefined>(undefined);
@@ -18,24 +19,19 @@ const SearchUserPage = () => {
         <div className={`container-fluid bg-light-lilac`}>
             <div className={`container my-4 py-2 rounded-3 bg-light-green`}>
                 <h1 className={`my-5 text-center text-dark-green`}>Programeri</h1>
-                <div className={`row`}>
                     {users && users.length > 0 ? (
                         users.map((user) => (
-                            <div key={user.id} className={`col-md-4 mb-3`}>
-                                <div className={`card`}>
-                                    <div className={`card-body text-center`}>
-                                        <h5 className={`text-violet`}>{user.username}</h5>
-                                        <p className={`text-lilac`}>{user.email}</p>
-                                    </div>
+                            <div key={user.id} className={`row mb-3`}>
+                                <div className={`col`}>
+                                    <UserCard user={user} />
                                 </div>
-                            </div>
-                        ))
+                        </div>
+                    ))
                     ) : (
                         <div className={`col-12`}>
                             <p className={`text-center text-muted`}>Nema korisnika za prikaz.</p>
                         </div>
                     )}
-                </div>
             </div>
         </div>
     );
---
+++ frontend/src/Components/UserCard/UserCard.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { User } from "../../Interfaces/User/User";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser} from "@fortawesome/free-solid-svg-icons";
+import styles from "./UserCard.module.css";
+
+type Props = { 
+    user:User;
+}
+
+const UserCard = ({ user }:Props) => {
+
+    return (
+        <div className={`card`}>
+            <div className={`card-body d-flex align-items-center`}>
+                {user.profileImage ? (
+                        <img 
+                            src={user.profileImage} 
+                            alt={`${user.username}'s profile`} 
+                            className={`rounded-circle me-3 ${styles.slika}`} 
+                        />
+                    ) : (
+                        <div 
+                            className={`rounded-circle bg-success d-flex justify-content-center align-items-center me-3 ${styles.slika}`} 
+                        >
+                            <FontAwesomeIcon icon={faUser} className={`text-white`} size="2x" />
+                        </div>
+                    )}
+                <div>
+                    <h5 className={`text-violet`}>{user.username}</h5>
+                    <p className={`text-lilac`}>{user.email}</p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default UserCard;
